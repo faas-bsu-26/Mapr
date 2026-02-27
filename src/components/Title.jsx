f627ef49d442ac47b58a7ea75be64c57797c5639
cburke6897 <cburke6897@gmail.com>
2026-02-27 18:29:34 -0500

    Fills background

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,13 +2,15 @@ import Logo from './Logo';
 
 export default function Title() {
   return (
-    <header className="relative w-full py-4 px-6 bg-title-bg">
-      <div className="absolute left-6 top-1/2 -translate-y-1/2">
-        <Logo />
+    <header className="relative w-full bg-title-bg">
+      <div className="py-4 px-6">
+        <div className="absolute left-6 top-1/2 -translate-y-1/2">
+          <Logo />
+        </div>
+        <h1 className="text-4xl font-bold text-center text-black">
+          Mapr
+        </h1>
       </div>
-      <h1 className="text-4xl font-bold text-center text-black">
-        Mapr
-      </h1>
     </header>
   );
 }
