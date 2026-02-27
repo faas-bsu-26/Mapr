bee6ad8cd005c423f46343581a49cd1848a365ab
cburke6897 <cburke6897@gmail.com>
2026-02-27 17:35:16 -0500

    Halves logo size

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import logo from '../assets/logo.png';
 
 export default function Logo() {
   return (
-    <div className="rounded-full overflow-hidden w-24 h-24">
+    <div className="rounded-full overflow-hidden w-12 h-12">
       <img 
         src={logo} 
         alt="Logo" 
