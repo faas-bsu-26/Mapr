4be67a0a3b884ef15f127ac9898805e1636e95de
cburke6897 <cburke6897@gmail.com>
2026-02-27 17:43:05 -0500

    Gives Title component a background color

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,11 +2,11 @@ import Logo from './Logo';
 
 export default function Title() {
   return (
-    <header className="relative w-full py-4 px-6">
+    <header className="relative w-full py-4 px-6 bg-title-bg">
       <div className="absolute left-6 top-1/2 -translate-y-1/2">
         <Logo />
       </div>
-      <h1 className="text-4xl font-bold text-center text-primary">
+      <h1 className="text-4xl font-bold text-center text-black">
         Mapr
       </h1>
     </header>
