3def042ad7a28bb04980eb7603d07e1b2ea01202
cburke6897 <cburke6897@gmail.com>
2026-02-27 17:26:59 -0500

    Creates Title component

---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import Logo from './Logo';
+
+export default function Title() {
+  return (
+    <header className="relative w-full py-4 px-6">
+      <div className="absolute left-6 top-1/2 -translate-y-1/2">
+        <Logo />
+      </div>
+      <h1 className="text-4xl font-bold text-center text-primary">
+        Mapr
+      </h1>
+    </header>
+  );
+}
