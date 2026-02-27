f2c5f47f60b31f4c1240d40f46496721e87f33a7
cburke6897 <cburke6897@gmail.com>
2026-02-27 17:44:01 -0500

    Darkens inner circle on hover

--- src/components/ScanButton.jsx
+++ src/components/ScanButton.jsx
@@ -1,8 +1,8 @@
 export default function ScanButton() {
   return (
-    <button className="relative w-24 h-24 flex items-center justify-center active:scale-95 transition-transform">
+    <button className="relative w-24 h-24 flex items-center justify-center active:scale-95 transition-transform group">
       <div className="absolute w-20 h-20 rounded-full bg-white border border-black"></div>
-      <div className="absolute w-16 h-16 rounded-full bg-white border border-black"></div>
+      <div className="absolute w-16 h-16 rounded-full bg-white group-hover:bg-neutral-300 border border-black transition-colors"></div>
     </button>
   );
 }
