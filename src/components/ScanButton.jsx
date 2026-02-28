efac4dee0a136c5aac113e71592fcc9abb51c9dc
cburke6897 <cburke6897@gmail.com>
2026-02-27 21:55:26 -0500

    Makes button navigate to Save page

--- src/components/ScanButton.jsx
+++ src/components/ScanButton.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from 'react-router-dom';
+
 export default function ScanButton() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/save');
+  };
+
   return (
-    <button className="relative w-24 h-24 flex items-center justify-center active:scale-95 transition-transform group">
+    <button onClick={handleClick} className="relative w-24 h-24 flex items-center justify-center active:scale-95 transition-transform group">
       <div className="absolute w-20 h-20 rounded-full bg-white border border-black"></div>
       <div className="absolute w-16 h-16 rounded-full bg-white group-hover:bg-neutral-300 group-active:bg-neutral-300 border border-black transition-colors"></div>
     </button>
