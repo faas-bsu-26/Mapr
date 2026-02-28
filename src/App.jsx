6021e96da76b0089bcbe47876a71001b74ddac19
cburke6897 <cburke6897@gmail.com>
2026-02-27 22:09:14 -0500

    Adds maplist route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 import Scan from './pages/Scan';
 import Save from './pages/Save';
+import MapList from './pages/MapList';
+import { MapProvider } from './context/MapContext';
 
 function App() {
   return (
-    <div className="min-h-screen flex justify-center bg-neutral-200">
-      <div className="w-full max-w-107.5 bg-white shadow-2xl">
-        <Routes>
-          <Route path="/" element={<Scan />} />
-          <Route path="/save" element={<Save />} />
-        </Routes>
+    <MapProvider>
+      <div className="min-h-screen flex justify-center bg-neutral-200">
+        <div className="w-full max-w-107.5 bg-white shadow-2xl">
+          <Routes>
+            <Route path="/" element={<Scan />} />
+            <Route path="/save" element={<Save />} />
+            <Route path="/maplist" element={<MapList />} />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </MapProvider>
   );
 }
 
