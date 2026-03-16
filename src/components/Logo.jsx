fc03e6595857f0200677610c39726df32773c5be
cburke6897 <cburke6897@gmail.com>
2026-03-16 13:02:54 -0400

    Makes clicking on Title elements navigate to maplist

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,13 @@
 import logo from '../assets/logo.png';
+import { useNavigate } from 'react-router-dom';
 
 export default function Logo() {
+  const navigate = useNavigate();
   return (
-    <div className="rounded-full overflow-hidden w-12 h-12">
+    <div
+      className="rounded-full overflow-hidden w-12 h-12 cursor-pointer transition hover:brightness-90 active:brightness-75"
+      onClick={() => navigate('/maplist')}
+    >
       <img 
         src={logo} 
         alt="Logo" 
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,13 +1,19 @@
 import Logo from './Logo';
+import { useNavigate } from 'react-router-dom';
 
 export default function Title() {
+  const navigate = useNavigate();
+
   return (
     <header className="relative w-full bg-title-bg">
       <div className="py-4 px-6">
         <div className="absolute left-6 top-1/2 -translate-y-1/2">
           <Logo />
         </div>
-        <h1 className="text-4xl font-bold text-center text-black">
+        <h1
+          className="text-4xl font-bold text-center text-black cursor-pointer transition-colors hover:text-zinc-700 active:text-zinc-600"
+          onClick={() => navigate('/maplist')}
+        >
           Mapr
         </h1>
       </div>
